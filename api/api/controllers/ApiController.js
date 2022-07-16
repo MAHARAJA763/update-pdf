f398db0820151f9950c4b26d54e19bb0ca488dcc
ashok bhati <ashok.bhati@techivies.com>
2022-07-16 19:03:11 +0530

    init update pdf changes

---
+++ api/api/controllers/ApiController.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const {PDFNet} = require('@pdftron/pdfnet-node');
+const moment = require('moment-timezone');
+
+module.exports = {
+  updatePDF: async (req, res) => {
+    if(typeof req.file('file') !== 'undefined') {
+      let file = req.file('file');
+      file.upload({dirname: path.normalize(__dirname + '../../../assets/pdf'), saveAs: (fileStream, callback) => {
+        callback(null, fileStream.filename);
+      }},(err, uploadedFiles) => {
+        if (err) {
+          return res.status(500).json(err);
+        } else {
+          let filePath = uploadedFiles[0].fd;
+          let outputPath = path.normalize(__dirname + '../../../assets/pdf/modified/' + uploadedFiles[0].filename)
+          let isDateFound = false;
+
+          const searchAndReplace = async () => {
+            const pdfdoc = await PDFNet.PDFDoc.createFromFilePath(filePath);
+            await pdfdoc.initSecurityHandler();
+            const txtSearch = await PDFNet.TextSearch.create();
+            let mode = PDFNet.TextSearch.Mode.e_reg_expression + PDFNet.TextSearch.Mode.e_page_stop
+            let pattern = "\\w+ \\d{1,2}\\w{2}, \\d{4}";
+
+            txtSearch.begin(pdfdoc, pattern, mode);
+
+            while(true){
+              const result = await txtSearch.run();
+              if(result.code === PDFNet.TextSearch.ResultCode.e_found){
+                isDateFound = true;
+                let newDate = moment(result.out_str, 'MMMM Do, YYYY').add(2, 'days').format('MMMM Do, YYYY');
+                const replacer = await PDFNet.ContentReplacer.create();
+                const page = await pdfdoc.getPage((result.page_num));
+                let arr = result.out_str.split(" ");
+                let arr2 = newDate.split(" ");
+                for (let i = 0; i < arr.length; i++) {
+                    let string1 = arr[i];
+                    let string2 = arr2[i];
+
+
+                    let matchStringStart = string1.substring(0, 1);
+                    let matchStringEnd = string1.substring(string1.length - 1);
+                    let replaceString = string1.substring(1, string1.length - 1);
+                    await replacer.setMatchStrings(matchStringStart, matchStringEnd);
+                    await replacer.addString(replaceString, string2);
+                    await replacer.process(page);
+                }
+                await pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
+              } else if(result.code === PDFNet.TextSearch.ResultCode.e_page){
+                console.log("\n Page End");
+                break;
+              } else if (result.code === PDFNet.TextSearch.ResultCode.e_done) {
+                console.log("\n Search End");
+                break;
+              }
+            }
+          }
+
+          PDFNet.runWithCleanup(searchAndReplace, 'demo:ashok.bhati@techivies.com:7a5e26e902000000001cffaa18934dadab5877453ef7b008928bef5d27').then(async () => {
+            await PDFNet.shutdown();
+            if(isDateFound){
+              return res.ok({filePath: outputPath});
+            } else {
+              res.status(400).json("no date found with format: MMMM Do, YYYY");
+            }
+          }).catch((err) => {
+            return res.status(500).json(err);
+          });
+        }
+      });
+    } else {
+      res.status(404).json({error:'file not found'});
+    }
+  }
+}
---
+++ update-pdf/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  file: any = {};
+  filePath: string = '';
+  error: string = '';
+
+  constructor(private apiService: ApiService){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onFileInput = ($event: any) => {
+    if ($event?.target?.files.length > 0) {
+      this.file = $event?.target?.files[0]
+    }
+  }
+
+  upload = () => {
+    this.filePath = "";
+    let formData = new FormData();
+    formData.append('file', this.file, this.file.name);
+    this.apiService.UpdatePdf(formData).subscribe((res: any) => {
+      if (res && res.filePath) {
+        this.filePath = res.filePath;
+      }
+    }, (err: any) => {
+      if(typeof err.error === 'string') {
+        this.error = err.error;
+      } else {
+        this.error = JSON.stringify(err.error);
+      }
+      console.log(`error ${JSON.stringify(err)}`);
+    });
+  }
+}
